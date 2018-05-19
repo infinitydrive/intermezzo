bdfd593e0d9093c934fc1026a421ba71f4e603f1
Charles Gerard McGovern <infinitydrive@Charless-MBP.lan>
2018-05-20 02:09:39 +1000

    stripe tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :items
   resources :charges
   root :to => "home#index"
+  get 'thanks', to: 'charges#thanks', as: 'thanks'
   
   #get "/items" => "items#index" #routes to list of items for sale
   #get "/items/:id" => "items#show" #routes to item sale page
