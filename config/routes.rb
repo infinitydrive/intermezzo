27c5e5af0289d747608495a32b85c2afc8978f72
Charles Gerard McGovern <infinitydrive@Charless-MBP.lan>
2018-05-20 00:34:23 +1000

    setup stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
+  resources :charges
   root :to => "home#index"
   
   #get "/items" => "items#index" #routes to list of items for sale
